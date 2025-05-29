da59d7915bc160b4c653ba2409d8b3006be23eb3
GabrielMS00 <gabriel.marquesdesouza.02@gmail.com>
2025-05-29 16:09:51 -0300

    Configuração inicial de ambiente

---
+++ tccTea/app/index.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+const Screen = () => {
+    return (
+        <SafeAreaView className="flex-1 items-center justify-center">
+            <Text className="font-semibold text-3xl">TCC TEA</Text>
+        </SafeAreaView>
+    );
+}
+
+export default Screen;
